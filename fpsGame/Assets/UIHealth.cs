32508c4fc72b872026636692ff5dad5bfe806b32
Ghostspartan141 <49250861+Ghostspartan141@users.noreply.github.com>
2024-03-25 11:25:27 -0500

    add sheilds and health pick ups and sword pick up

--- fpsGame/Assets/UIHealth.cs
+++ fpsGame/Assets/UIHealth.cs
@@ -5,7 +5,11 @@ using UnityEngine.UI;
 public class UIHealth : MonoBehaviour
 {
     public Image healthBar;
+    public Image sheildbar;
     public float healthAmount = 100f;
+    public float sheildpower=0f;
+    public GameObject sheildpickup;
+    public GameObject healthPickup;
 
     // Start is called before the first frame update
     void Start()
@@ -25,6 +29,11 @@ public class UIHealth : MonoBehaviour
     //takes damage
     public void TakeDamage(float Damage)
     {
+        if(sheildpower>0)
+        {
+            sheildpower-=Damage;
+            sheildbar.fillAmount = sheildpower / 100f;
+        }
         healthAmount -= Damage;
         healthBar.fillAmount = healthAmount / 100f;
     }
@@ -32,20 +41,46 @@ public class UIHealth : MonoBehaviour
     public void TakeHeal(float Heal)
     {
         healthAmount += Heal;
+        if (healthAmount>=100)
+        {
+            sheildpower = healthAmount - 100;
+            sheildbar.fillAmount = sheildpower / 100f;
+            healthAmount = 100f;
+            Destroy(healthPickup);
+        }
+        
         healthBar.fillAmount = Mathf.Clamp(Heal, 0, 100);
         healthBar.fillAmount = healthAmount / 100f;
+        Destroy(healthPickup);
+    }
+    public void sheildhealth(float Heal)
+    {
+        sheildpower += Heal;
+        if (sheildpower>100)
+        {
+            sheildpower = 100f;
+        }
+        
+        sheildbar.fillAmount = Mathf.Clamp(Heal, 0, 100);
+        sheildbar.fillAmount = healthAmount / 100f;
+        Destroy(sheildpickup);
     }
     // checks collison
     public void OnTriggerStay(Collider other)
     {
         if(other.tag =="enemy")
         {
-            TakeDamage(1);
+            TakeDamage(15);
         }
-        if (other.tag == "healthpad")
+        if (other.tag == "healthkit")
         {
-            TakeHeal(1);
+            TakeHeal(20);
         }
+        if(other.tag=="sheildpickup")
+        {
+            sheildhealth(100);
+        }
+       
     }
 }
 
